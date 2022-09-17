635fc0ca9a16b05b95ac1690bc0924b50b0a9d39
Vadim Tentyuk <vadim.tentyuk97@gmail.com>
2022-09-17 20:53:50 +1000

    rebuild

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -14,7 +14,7 @@ public class App implements Callable<Integer> {
     @Parameters(index = "0", description = "path to first file")
     private String filepath1;
 
-    @Parameters(index = "2", description = "path to second file")
+    @Parameters(index = "1", description = "path to second file")
     private String filepath2;
 
     @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", defaultValue = "stylish")
