0107eeaa99717ddf4bca0fe447ee2ad55d62a132
Vadim Tentyuk <vadim.tentyuk97@gmail.com>
2022-09-18 21:26:08 +1000

    rebuild

--- src/test/java/hexlet/code/Tests.java
+++ src/test/java/hexlet/code/Tests.java
@@ -5,37 +5,35 @@ import org.junit.jupiter.api.BeforeAll;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Tests {
-    
     private static String firstResult;
     private static String secondResult;
     private static String thirdResult;
-    
     @BeforeAll
     public static void beforeAll() throws Exception {
-        firstResult = "{\n" +
-                " - Chords: [c, d, e]\n" +
-                " - Good song?: true\n" +
-                " + Good song?: false\n" +
-                " - Haters: null\n" +
-                " + Haters: 5\n" +
-                " - Song: Best kept secret\n" +
-                " + Song: Wrecking ball\n" +
-                "   Year of song: 2003\n" +
-                "}";
-        secondResult = "{\n" +
-                " - Chords: [c, d, e]\n" +
-                " - Good song?: true\n" +
-                " - Haters: null\n" +
-                " - Song: Best kept secret\n" +
-                " - Year of song: 2003\n" +
-                "}";
-        thirdResult = "{\n" +
-                " + Chords: [c, d, e]\n" +
-                " + Good song?: true\n" +
-                " + Haters: null\n" +
-                " + Song: Best kept secret\n" +
-                " + Year of song: 2003\n" +
-                "}";
+        firstResult = "{\n"
+                + " - Chords: [c, d, e]\n"
+                + " - Good song?: true\n"
+                + " + Good song?: false\n"
+                + " - Haters: null\n"
+                + " + Haters: 5\n"
+                + " - Song: Best kept secret\n"
+                + " + Song: Wrecking ball\n"
+                + "   Year of song: 2003\n"
+                + "}";
+        secondResult = "{\n"
+                + " - Chords: [c, d, e]\n"
+                + " - Good song?: true\n"
+                + " - Haters: null\n"
+                + " - Song: Best kept secret\n"
+                + " - Year of song: 2003\n"
+                + "}";
+        thirdResult = "{\n"
+                + " + Chords: [c, d, e]\n"
+                + " + Good song?: true\n"
+                + " + Haters: null\n"
+                + " + Song: Best kept secret\n"
+                + " + Year of song: 2003\n"
+                + "}";
     }
 
     @Test
